ca6efbfa674b3aa153a0f5d6c345cffd749c0a90
Silke <silke@slxh.eu>
2018-02-07 14:18:03 +0100

    Update test vectors

--- ipcipher_test.go
+++ ipcipher_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 var keyTestVectors = map[string][]byte{
-	"":                     {0x99, 0xbe, 0x12, 0x3a, 0xc5, 0xf8, 0x67, 0xdb, 0x37, 0x19, 0x3d, 0xb7, 0xae, 0xe6, 0x7e, 0x73},
-	"3.141592653589793":    {0x23, 0x07, 0x23, 0x58, 0xad, 0xcb, 0x9b, 0x23, 0x05, 0x57, 0x4e, 0x23, 0x29, 0x1b, 0x40, 0xad},
-	"crypto is not a coin": {0xc8, 0x18, 0x0e, 0x56, 0x05, 0x6d, 0x3e, 0xcb, 0xf8, 0x50, 0x50, 0x0b, 0xfd, 0x84, 0x19, 0x3d},
+	"":                     {0xbb, 0x8d, 0xcd, 0x7b, 0xe9, 0xa6, 0xf4, 0x3b, 0x33, 0x04, 0xc6, 0x40, 0xd7, 0xd7, 0x10, 0x3c},
+	"3.141592653589793":    {0x37, 0x05, 0xbd, 0x6c, 0x0e, 0x26, 0xa1, 0xa8, 0x39, 0x89, 0x8f, 0x1f, 0xa0, 0x16, 0xa3, 0x74},
+	"crypto is not a coin": {0x06, 0xc4, 0xba, 0xd2, 0x3a, 0x38, 0xb9, 0xe0, 0xad, 0x9d, 0x05, 0x90, 0xb0, 0xa3, 0xd9, 0x3a},
 }
 
-var ipv4TestVectors = map[*Key]map[string]string{
+var testVectors = map[*Key]map[string]string{
 	&Key{'s', 'o', 'm', 'e', ' ', '1', '6', '-', 'b', 'y', 't', 'e', ' ', 'k', 'e', 'y'}: {
 		"127.0.0.1": "114.62.227.59",
 		"8.8.8.8":   "46.48.51.50",
@@ -22,16 +22,16 @@ var ipv4TestVectors = map[*Key]map[string]string{
 		"2001:DB8::":          "ce7e:7e39:d282:e7b1:1d6d:5ca1:d4de:246f",
 	},
 	NewKeyFromPassword("crypto is not a coin"): {
-		"198.41.0.4":    "78.178.254.81",
-		"130.161.180.1": "207.193.250.137",
-		"0.0.0.0":       "134.197.67.89",
-		"::1":           "2ec1:fa64:6771:a68b:dcb:6cca:8422:5c1c",
-		"2001:503:ba3e::2:30": "d8a9:27d7:b9d1:492f:670e:6ffc:e427:fe49",
-		"2001:DB8::":          "6709:bdb1:cd1e:354f:ebfb:5775:fb51:8e64",
+		"198.41.0.4":    "139.111.117.167",
+		"130.161.180.1": "66.235.221.231",
+		"0.0.0.0":       "203.253.152.187",
+		"::1":           "a551:9cb0:c9b:f6e1:6112:58a:af29:3a6c",
+		"2001:503:ba3e::2:30": "6e60:2674:2fac:d383:f9d5:dcfe:fc53:328e",
+		"2001:DB8::":          "a8f5:16c8:e2ea:23b9:748d:67a2:4107:9d2e",
 	},
 }
 
-var ipv4MegaTestVectors = map[*Key]map[string]string{
+var megaTestVectors = map[*Key]map[string]string{
 	&Key{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16}: {
 		"192.168.69.42": "93.155.197.186",
 	},
@@ -47,7 +47,7 @@ func TestKeyGeneration(t *testing.T) {
 }
 
 func TestEncrypt(t *testing.T) {
-	for key, ips := range ipv4TestVectors {
+	for key, ips := range testVectors {
 		for in, out := range ips {
 			o, _ := Encrypt(key, net.ParseIP(in))
 			if !bytes.Equal(o, net.ParseIP(out)) {
@@ -58,7 +58,7 @@ func TestEncrypt(t *testing.T) {
 }
 
 func TestDecrypt(t *testing.T) {
-	for key, ips := range ipv4TestVectors {
+	for key, ips := range testVectors {
 		for out, in := range ips {
 			o, _ := Decrypt(key, net.ParseIP(in))
 			if !bytes.Equal(o, net.ParseIP(out)) {
@@ -68,8 +68,8 @@ func TestDecrypt(t *testing.T) {
 	}
 }
 
-func TestEncryptIPv4Mega(t *testing.T) {
-	for key, ips := range ipv4MegaTestVectors {
+func TestEncryptMega(t *testing.T) {
+	for key, ips := range megaTestVectors {
 		for in, out := range ips {
 			o := net.ParseIP(in)
 			for i := 0; i < 100000000; i++ {
